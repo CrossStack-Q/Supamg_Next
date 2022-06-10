b17150050110396ce424a94a537439cab76ab640
CrossStack-Q <crossstack@gmail.com>
2022-06-10 03:59:21 -0700

    Add on-demand ISR.

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ function Gallery({images}:{images: Image[]}) {
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {/* Images will go here */}
         {images.map((image) => (
-          <BlurImage key={images.id} image={image} />
+          <BlurImage key={image.id} image={image} />
         ))}
         
       </div>
